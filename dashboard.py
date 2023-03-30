a94a1b6f1e633043bdd09b8f19bf1c4a545d181e
red_hoodie <rudrakshigupta305@gmail.com>
2023-03-30 20:33:20 +0530

    Basics

--- dashboard.py
+++ dashboard.py
@@ -1,2 +1,11 @@
-import tkinter as Tk;
+from tkinter import *
 
+class face:
+    def __init__(self,window):
+        self.window = window
+        self.window.title("Web Scrapper - Times of India!")
+        self.window.geometry('1200x675+0+0')
+
+window = Tk()
+player = face(window)
+window.mainloop()

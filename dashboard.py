f06a1b933d0a2f1acb99e212a8594a0a6c330793
red_hoodie <rudrakshigupta305@gmail.com>
2023-03-31 18:55:38 +0530

    Dashboard completed

--- dashboard.py
+++ dashboard.py
@@ -7,10 +7,13 @@ class face:
         self.window.title("AWS User Automation App")
         self.window.geometry('1200x675+0+0')
 
-        Boardframe = LabelFrame(self.window,bd=20,bg="white",text ="AWS-IAM User Recommendation System",relief=FLAT,font=('calibri',12))
+        Boardframe = LabelFrame(self.window,bd=20,bg="black",text ="AWS-IAM User Recommendation System",foreground="white",relief=FLAT,font=('calibri',33))
         Boardframe.place(x=120,y=80,width=900,height=80)
-        text_b = Text(Boardframe,width=800, height=595, font=('calibri',14),blockcursor=True,relief=FLAT)
+        text_b = Text(Boardframe,width=800, height=595, font=('calibri',12),blockcursor=True,relief=FLAT)
         text_b.pack()
+
+        b1 = Button(window, text = 'User Information',bg='papaya whip',cursor='hand2' ,relief=FLAT, foreground='black', width=35, height=4,activebackground='light gray', command=lambda : user('u')).place(x=450, y= 270)#add,display and edit
+        b2 = Button(window, text = 'Roles & Policies', bg='peach puff',cursor='hand2',relief=FLAT, foreground='black', width=35, height=4,activebackground='light gray', command=lambda: rnp('r')).place(x=450, y= 370)#add and dislay
 
 window = Tk()
 
